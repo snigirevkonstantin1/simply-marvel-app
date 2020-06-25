7f6c875e9114062c0e2fa6aaf7470c545b029ca2
snigirevkonstantin1 <snigirevkonstantin1@gmail.com>
2020-06-25 16:57:20 +0300

    Updates

---
+++ precache-manifest.4d1f1ec88af389e3e12ad7671bd6394c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb379e36729a5cf51a8025e258f36f8e",
+    "url": "/simply-marvel-app/index.html"
+  },
+  {
+    "revision": "30d950ff4526abe7e538",
+    "url": "/simply-marvel-app/static/css/2.9109016a.chunk.css"
+  },
+  {
+    "revision": "947ab61f5b3320495296",
+    "url": "/simply-marvel-app/static/css/main.ebeab49f.chunk.css"
+  },
+  {
+    "revision": "30d950ff4526abe7e538",
+    "url": "/simply-marvel-app/static/js/2.1a11ebf7.chunk.js"
+  },
+  {
+    "revision": "c18ad82b3be9985ffcbc3de3c04c0198",
+    "url": "/simply-marvel-app/static/js/2.1a11ebf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947ab61f5b3320495296",
+    "url": "/simply-marvel-app/static/js/main.383bf5bf.chunk.js"
+  },
+  {
+    "revision": "5a4b6c3c87a79ac92746",
+    "url": "/simply-marvel-app/static/js/runtime-main.22a737cd.js"
+  }
+]);
